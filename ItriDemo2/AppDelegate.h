f91308d037085a9fbf487ef79f4a0bbf3c40a152
Mark Ho <mark@patristar.com>
2013-02-22 15:41:26 +0800

    Initial Commit

---
+++ ItriDemo2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ItriDemo2
+//
+//  Created by Ho Mark on 13/2/22.
+//  Copyright (c) 2013年 Ho Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
